cbf83944ffd173fd823fe40e398eb9ff9f82da05
Willian Marciano <willian_marciano@hotmail.com>
2022-04-14 18:06:03 -0300

    Incluido teste VerificarQuantidadeDeVezesTotalTarefasAtrasadas

--- tests/CadastrarTarefas.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
+++ tests/CadastrarTarefas.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
@@ -4,6 +4,7 @@ using CadastrarTarefas.Infrastructure;
 using CadastrarTarefas.Services.Handlers;
 using CadastrarTarefas.Testes.Configure;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,6 +49,33 @@ namespace CadastrarTarefas.Testes
             //assert
             var tarefasEmAtraso = repo.ObtemTarefas(t => t.Status == StatusTarefa.EmAtraso);
             Assert.Equal(5, tarefasEmAtraso.Count());
+        }
+
+        [Fact]
+        public void VerificarQuantidadeDeVezesTotalTarefasAtrasadas()
+        {
+            //arrange
+            var categ = new Categoria("Dummy");
+            var tarefas = new List<Tarefa>
+            {
+                new Tarefa(1, "Tirar Lixo",    categ, new DateTime(2022,01,01), null, StatusTarefa.Criada),
+                new Tarefa(2, "Fazer Almoçco", categ, new DateTime(2022,01,01), null, StatusTarefa.Criada),
+                new Tarefa(3, "Ir à academia", categ, new DateTime(2022,01,01), null, StatusTarefa.Criada),
+            };
+
+            var mock = new Mock<IRepositorioTarefas>();
+            mock.Setup(r => r.ObtemTarefas(It.IsAny<Func<Tarefa, bool>>()))
+                .Returns(tarefas);
+            var repo = mock.Object;
+
+            var comando = new GerenciaPrazoDasTarefas(new DateTime(2022, 4, 14));
+            var handler = new GerenciaPrazoDasTarefasHandler(repo);
+
+            //act
+            handler.Execute(comando);
+
+            //assert
+            mock.Verify(r => r.AtualizarTarefas(It.IsAny<Tarefa[]>()), Times.Once());
         }
     }
 }
