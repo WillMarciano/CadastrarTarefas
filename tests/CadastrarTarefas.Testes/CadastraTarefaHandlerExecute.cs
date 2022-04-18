ac7f2ddf55c3812b732d44b03db4e561056bc3c1
Willian Marciano <willian_marciano@hotmail.com>
2022-04-18 14:51:08 -0300

    Mocka objetos complexos

--- tests/CadastrarTarefas.Testes/CadastraTarefaHandlerExecute.cs
+++ tests/CadastrarTarefas.Testes/CadastraTarefaHandlerExecute.cs
@@ -6,6 +6,7 @@ using CadastrarTarefas.Testes.Configure;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -37,6 +38,50 @@ namespace CadastrarTarefas.Testes
         }
 
         [Fact]
+        public void DadaTarefaComInfoValidasDeveLogar()
+        {
+            //arrange            
+            var tituloTarefaEsperado = "Usar Moq para aprofundar conhecimento de API";
+            var comando = new CadastraTarefa(tituloTarefaEsperado, new Categoria(100, "Estudo"), new DateTime(2019, 12, 31));
+
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            LogLevel levelCapturado = LogLevel.Error;
+            string mensagemCapturada = string.Empty;
+
+            mockLogger.Setup(
+                l => l.Log(
+                    LogLevel.Debug,
+                    It.IsAny<EventId>(),
+                    It.IsAny<It.IsAnyType>(),
+                    It.IsAny<Exception>(),
+                    (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()))
+                .Callback((IInvocation invocation) =>
+                {
+                    var logLevel = (LogLevel)invocation.Arguments[0];
+                    var eventId = (EventId)invocation.Arguments[1];
+                    var state = (IReadOnlyCollection<KeyValuePair<string, object>>)invocation.Arguments[2];
+                    var exception = invocation.Arguments[3] as Exception;
+                    var formatter = invocation.Arguments[4] as Delegate;
+                    var formatterStr = formatter.DynamicInvoke(state, exception);
+
+                    levelCapturado = logLevel;
+                    mensagemCapturada = formatterStr.ToString();
+                });
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            var handler = new CadastraTarefaHandler(mock.Object, mockLogger.Object);
+
+            //act
+            handler.Execute(comando); //SUT >> CadastraTarefaHandlerExecute
+
+            //assert
+            Assert.Equal(LogLevel.Debug, levelCapturado);
+            Assert.Contains(tituloTarefaEsperado, mensagemCapturada);
+        }
+
+        [Fact]
         public void SeHouverExceptionResultadoIsSuccessDeveSerFalso()
         {
             //arrange
@@ -58,6 +103,8 @@ namespace CadastrarTarefas.Testes
             //assert
             Assert.False(resultado.IsSuccess);
         }
+
+
 
         [Fact]
         public void SeHouverExceptionDeveLogarMensagemDaExcecao()
