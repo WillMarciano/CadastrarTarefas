4ab4e04161aff29ccb6447feca1c1db4d22a69e3
Willian Marciano <willian_marciano@hotmail.com>
2022-04-18 10:58:40 -0300

    Incluido SeIdForExistenteChamarObtemCategoriaPorIdUmaUnicaVez

---
+++ tests/CadastrarTarefas.Testes/ObtemCategoriaPorIdExecute.cs
@@ -0,0 +1,28 @@
+﻿using CadastrarTarefas.Core.Commands;
+using CadastrarTarefas.Infrastructure;
+using CadastrarTarefas.Services.Handlers;
+using Moq;
+using Xunit;
+
+namespace CadastrarTarefas.Testes
+{
+    public class ObtemCategoriaPorIdExecute
+    {
+        [Fact]
+        public void SeIdForExistenteChamarObtemCategoriaPorIdUmaUnicaVez()
+        {
+            //arrange
+            var idCategoria = 20;
+            var comando = new ObtemCategoriaPorId(idCategoria);
+            var mock = new Mock<IRepositorioTarefas>();
+            var repo = mock.Object;
+            var handler = new ObtemCategoriaPorIdHandler(repo);
+
+            //act
+            handler.Execute(comando);
+
+            //execute
+            mock.Verify(r => r.ObtemCategoriaPorId(idCategoria), Times.Once());
+        }
+    }
+}
